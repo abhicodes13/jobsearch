e526c9e86ab8a4341cd0d5bf2ab1e01c2af2d421
AbhiCodes <chhettriabhishek9@gmail.com>
2023-10-14 21:41:10 -0400

    job component completion

--- src/Components/Jobs.jsx
+++ src/Components/Jobs.jsx
@@ -12,9 +12,43 @@ const Jobs = () => {
             </span>
         </span>
         <h6 className="text-[#ccc]">Canada</h6>
-        <p className="text-sm hover:text-md">Lorem ipsum, dolor sit amet coedit sum, dolor sit amet coedit sum, dolor sit amet coedit</p>
+        <p className="text-sm">Lorem ipsum,coeditcoedit dolocoedit rcoedit sit amet coedit sum, dolor sit amet coedit sum, dolor sit amet coedit</p>
+        </div>
+
+        <div className="group group/item singleJob w-[250px] p-[20px] bg-white rounded-[10px] hover:bg-blueColor shadow-lg shadow-greyIsh-400/700 hover:shadow-lg">
+        <span className='flex justify-between items-center gap-4'>
+            <h1 className='text-[16px] font-semibold text-textColor group-hover:text-white'>UI/UX Developer</h1>
+            <span className='flex items-center text-[#ccc] gap-1'>
+                <BiTimeFive/>1month
+            </span>
+        </span>
+        <h6 className="text-[#ccc]">USA</h6>
+        <p className="text-sm"> amet coedit sum, dolor sit asum, dolor sum, dolor sitsitsum, dolor sitsum, dolor sitmet coedit</p>
+        </div>
+
+        <div className="group group/item singleJob w-[250px] p-[20px] bg-white rounded-[10px] hover:bg-blueColor shadow-lg shadow-greyIsh-400/700 hover:shadow-lg">
+        <span className='flex justify-between items-center gap-4'>
+            <h1 className='text-[16px] font-semibold text-textColor group-hover:text-white'>Fullstack Developer</h1>
+            <span className='flex items-center text-[#ccc] gap-1'>
+                <BiTimeFive/>2weeks
+            </span>
+        </span>
+        <h6 className="text-[#ccc]">USA</h6>
+        <p className="text-sm">Lorem ipsum, dolor sit amet coedit sum, dolor sit amet coedit sum, dolor sit amet coedit</p>
+        </div>
+
+        <div className="group group/item singleJob w-[250px] p-[20px] bg-white rounded-[10px] hover:bg-blueColor shadow-lg shadow-greyIsh-400/700 hover:shadow-lg">
+        <span className='flex justify-between items-center gap-4'>
+            <h1 className='text-[16px] font-semibold text-textColor group-hover:text-white'>Email Developer</h1>
+            <span className='flex items-center text-[#ccc] gap-1'>
+                <BiTimeFive/>Now
+            </span>
+        </span>
+        <h6 className="text-[#ccc]">Switzerland</h6>
+        <p className="text-sm">Lorem ipsum, dolor sit amet coedit sum, dolor sit ametcoedit coedit coeditcoeditsum, dolor coeditsit amet coedit</p>
         </div>
     </div>
+     
   )
 }
 
