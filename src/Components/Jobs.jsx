5f0a7a311b2c08cbc21bed1c387b8d7273099bac
AbhiCodes <chhettriabhishek9@gmail.com>
2023-10-14 21:34:50 -0400

    job component

--- src/Components/Jobs.jsx
+++ src/Components/Jobs.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import {BiTimeFive} from 'react-icons/bi'
 
 const Jobs = () => {
   return (
-    <div>Jobs</div>
+    <div className='jobContainer flex gap-10 justify-center flex-wrap items-center py-10'>
+        <div className="group group/item singleJob w-[250px] p-[20px] bg-white rounded-[10px] hover:bg-blueColor shadow-lg shadow-greyIsh-400/700 hover:shadow-lg">
+        <span className='flex justify-between items-center gap-4'>
+            <h1 className='text-[16px] font-semibold text-textColor group-hover:text-white'>Web Developer</h1>
+            <span className='flex items-center text-[#ccc] gap-1'>
+                <BiTimeFive/>Now
+            </span>
+        </span>
+        <h6 className="text-[#ccc]">Canada</h6>
+        <p className="text-sm hover:text-md">Lorem ipsum, dolor sit amet coedit sum, dolor sit amet coedit sum, dolor sit amet coedit</p>
+        </div>
+    </div>
   )
 }
 
